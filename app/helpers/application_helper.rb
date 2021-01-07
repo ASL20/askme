bf4cfd2b6b07e74531a59322e17e53919de08188
Anton <anton.lipich1@gmail.com>
2021-01-07 20:16:52 +0300

    Add helper with questions quantity

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,22 @@ module ApplicationHelper
       asset_path 'avatar.jpg'
     end
   end
+
+  def questions_quantity(questions)
+    quantity = questions.size
+
+    if quantity == nil || !quantity.is_a?(Numeric)
+      number = 0
+    end
+
+    remainder = quantity % 10
+
+    if !remainder.between?(1, 4) || (quantity % 100).between?(11, 14)
+      "#{quantity} вопросов"
+    elsif remainder.between?(2, 4)
+      "#{quantity} вопроса"
+    else
+      "#{quantity} вопрос"
+    end
+  end
 end
