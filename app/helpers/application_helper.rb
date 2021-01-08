9edf6552fba0a8d182b15d142ea30cbb38204760
Anton <anton.lipich1@gmail.com>
2021-01-08 18:42:17 +0300

    Add display of questions with and without answers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,20 +8,27 @@ module ApplicationHelper
   end
 
   def questions_quantity(questions)
-    quantity = questions.size
+    arr = [
+      questions_with_answers = questions.select(&:answer).size,
+      questions_without_answers = questions.reject(&:answer).size
+    ]
+    result = []
 
-    if quantity == nil || !quantity.is_a?(Numeric)
-      number = 0
+    arr.each do |quantity|
+      if quantity == nil || !quantity.is_a?(Numeric)
+        number = 0
+      end
+
+      remainder = quantity % 10
+
+      if !remainder.between?(1, 4) || (quantity % 100).between?(11, 14)
+        result << "#{quantity} вопросов"
+      elsif remainder.between?(2, 4)
+        result << "#{quantity} вопроса"
+      else
+        result << "#{quantity} вопрос"
+      end
     end
-
-    remainder = quantity % 10
-
-    if !remainder.between?(1, 4) || (quantity % 100).between?(11, 14)
-      "#{quantity} вопросов"
-    elsif remainder.between?(2, 4)
-      "#{quantity} вопроса"
-    else
-      "#{quantity} вопрос"
-    end
+    result
   end
 end
