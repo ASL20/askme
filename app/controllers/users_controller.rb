127d4a8ee4c1e329538139d0d3eab8654e158cc9
Anton Lipich <anton.lipich1@gmail.com>
2021-01-18 20:57:17 +0300

    add User authorization after registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   before_action :load_user, except: [:index, :create, :new]
   before_action :authorize_user, except: [:index, :new, :create, :show]
 
@@ -14,11 +13,11 @@ class UsersController < ApplicationController
 
   def create
     redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
-
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
+      session[:user_id] = @user.id
+      redirect_to user_path(@user), notice: 'Регистрация прошла успешно!'
     else
       render 'new'
     end
