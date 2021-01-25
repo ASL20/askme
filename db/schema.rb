46460ec9f4b865de7ce33660b0fb94dbd7edb4b2
Anton Lipich <anton.lipich1@gmail.com>
2021-01-25 13:33:31 +0300

    fix anonymous questions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_24_105553) do
+ActiveRecord::Schema.define(version: 2021_01_24_122137) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 2021_01_24_105553) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
     t.integer "author_id"
-    t.index ["author_id"], name: "index_questions_on_author_id"
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
